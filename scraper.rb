95e4785edc4cd7aa8a600d67b15c989edd2c03d3
Tony Bowden <tony@tmtm.com>
2016-05-21 14:28:48 +0100

    Add earlier terms

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,14 @@ require 'wikidata/fetcher'
 urls = [
   'Members of the Australian Senate, 2014–2017',
   'Members of the Australian Senate, 2011–2014',
+  'Members of the Australian Senate, 2008–2011',
+  'Members of the Australian Senate, 2005–2008',
+  'Members of the Australian Senate, 2002–2005',
+  'Members of the Australian Senate, 1999–2002',
+  'Members of the Australian Senate, 1996–1999',
+  'Members of the Australian Senate, 1993–1996',
+  'Members of the Australian Senate, 1990–1993',
+  'Members of the Australian Senate, 1987–1990',
 ]
 
 names = urls.map do |url|
